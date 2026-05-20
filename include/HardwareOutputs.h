c103b025370971e5d69740e43c57de693d55d156
bdkazal <bdkazal@gmail.com>
2026-05-20 21:12:00 +0600

    Track pump low speed assist timing

--- include/HardwareOutputs.h
+++ include/HardwareOutputs.h
@@ -21,10 +21,12 @@ private:
   bool hardwareEnabled = false;
   bool hasLoggedPumpLevel = false;
   bool lastPumpOn = false;
+  bool lastPumpAssistActive = false;
   int lastPumpSpeedPercent = -1;
   int lastPumpDuty = -1;
   bool pumpStartupBoostActive = false;
   unsigned long pumpStartupBoostUntil = 0;
+  unsigned long lastPumpAssistKickAt = 0;
 
   void applyPump(const FountainOutputState &outputs);
   void applyCob(const FountainOutputState &outputs);
