c5526c8afd928d7ef415f5b928ddb50935486b53
bdkazal <bdkazal@gmail.com>
2026-05-20 18:24:18 +0600

    Use debounced float switch for water safety

--- src/WaterLevelSensor.cpp
+++ src/WaterLevelSensor.cpp
@@ -1,5 +1,8 @@
 #include "WaterLevelSensor.h"
+
+#if __has_include("DevFlags.h")
 #include "DevFlags.h"
+#endif
 
 #if __has_include("HardwarePins.h")
 #include "HardwarePins.h"
@@ -23,6 +26,10 @@
 #define WATER_LEVEL_SWITCH_USE_PULLUP 1
 #endif
 
+#ifndef WATER_LEVEL_SWITCH_DEBOUNCE_MS
+#define WATER_LEVEL_SWITCH_DEBOUNCE_MS 150
+#endif
+
 void WaterLevelSensor::begin()
 {
   switchConfigured = WATER_LEVEL_SWITCH_PIN >= 0;
@@ -42,17 +49,23 @@ void WaterLevelSensor::begin()
     pinMode(WATER_LEVEL_SWITCH_PIN, INPUT);
   }
 
+  lastRawSwitchWaterLow = readSwitchWaterLow();
+  debouncedWaterLow = lastRawSwitchWaterLow;
+  lastRawSwitchChangedAt = millis();
+
   Serial.print("WaterLevelSensor switch ready: GPIO ");
   Serial.print(WATER_LEVEL_SWITCH_PIN);
   Serial.print(" active_");
-  Serial.println(WATER_LEVEL_SWITCH_ACTIVE_LOW == 1 ? "LOW" : "HIGH");
+  Serial.print(WATER_LEVEL_SWITCH_ACTIVE_LOW == 1 ? "LOW" : "HIGH");
+  Serial.print(" debounce_ms=");
+  Serial.println(WATER_LEVEL_SWITCH_DEBOUNCE_MS);
 }
 
 void WaterLevelSensor::update(FountainReadings &readings)
 {
   if (switchConfigured)
   {
-    applyWaterLowState(readings, readSwitchWaterLow(), "switch");
+    applyWaterLowState(readings, debouncedSwitchWaterLow(), "switch");
     return;
   }
 
@@ -69,6 +82,25 @@ bool WaterLevelSensor::readSwitchWaterLow() const
   }
 
   return pinState == HIGH;
+}
+
+bool WaterLevelSensor::debouncedSwitchWaterLow()
+{
+  bool rawWaterLow = readSwitchWaterLow();
+  unsigned long now = millis();
+
+  if (rawWaterLow != lastRawSwitchWaterLow)
+  {
+    lastRawSwitchWaterLow = rawWaterLow;
+    lastRawSwitchChangedAt = now;
+  }
+
+  if (debouncedWaterLow != rawWaterLow && now - lastRawSwitchChangedAt >= WATER_LEVEL_SWITCH_DEBOUNCE_MS)
+  {
+    debouncedWaterLow = rawWaterLow;
+  }
+
+  return debouncedWaterLow;
 }
 
 void WaterLevelSensor::applyWaterLowState(FountainReadings &readings, bool waterLow, const char *source)
