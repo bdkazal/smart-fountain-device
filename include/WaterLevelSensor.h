183f8ebd79ed396b5f8c9f733fbb2668b1aacca3
bdkazal <bdkazal@gmail.com>
2026-05-20 18:13:34 +0600

    Track water level switch state

--- include/WaterLevelSensor.h
+++ include/WaterLevelSensor.h
@@ -8,4 +8,12 @@ class WaterLevelSensor
 public:
   void begin();
   void update(FountainReadings &readings);
+
+private:
+  bool switchConfigured = false;
+  bool lastWaterLow = false;
+  bool hasLoggedInitialState = false;
+
+  bool readSwitchWaterLow() const;
+  void applyWaterLowState(FountainReadings &readings, bool waterLow, const char *source);
 };
