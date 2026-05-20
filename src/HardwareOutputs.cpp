b93bf66fa5f5cb16b172537fd4dfb1cc8ded2558
bdkazal <bdkazal@gmail.com>
2026-05-20 20:29:42 +0600

    Add pump minimum duty mapping and startup boost

--- src/HardwareOutputs.cpp
+++ src/HardwareOutputs.cpp
@@ -19,11 +19,27 @@
 #endif
 
 #ifndef PUMP_PWM_FREQUENCY
-#define PUMP_PWM_FREQUENCY 20000
+#define PUMP_PWM_FREQUENCY 1000
 #endif
 
 #ifndef PUMP_PWM_RESOLUTION_BITS
 #define PUMP_PWM_RESOLUTION_BITS 8
+#endif
+
+#ifndef PUMP_PWM_MIN_DUTY_PERCENT
+#define PUMP_PWM_MIN_DUTY_PERCENT 10
+#endif
+
+#ifndef PUMP_PWM_MAX_DUTY_PERCENT
+#define PUMP_PWM_MAX_DUTY_PERCENT 100
+#endif
+
+#ifndef PUMP_STARTUP_BOOST_DUTY_PERCENT
+#define PUMP_STARTUP_BOOST_DUTY_PERCENT 100
+#endif
+
+#ifndef PUMP_STARTUP_BOOST_MS
+#define PUMP_STARTUP_BOOST_MS 700
 #endif
 
 void HardwareOutputs::begin()
@@ -51,7 +67,13 @@ void HardwareOutputs::begin()
       Serial.print(" freq=");
       Serial.print(PUMP_PWM_FREQUENCY);
       Serial.print("Hz active_");
-      Serial.println(PUMP_OUTPUT_ACTIVE_HIGH == 1 ? "HIGH" : "LOW");
+      Serial.print(PUMP_OUTPUT_ACTIVE_HIGH == 1 ? "HIGH" : "LOW");
+      Serial.print(" min=");
+      Serial.print(PUMP_PWM_MIN_DUTY_PERCENT);
+      Serial.print(" max=");
+      Serial.print(PUMP_PWM_MAX_DUTY_PERCENT);
+      Serial.print(" boost_ms=");
+      Serial.println(PUMP_STARTUP_BOOST_MS);
     }
     else
     {
@@ -109,21 +131,56 @@ void HardwareOutputs::applyPump(const FountainOutputState &outputs)
   }
 
   bool pumpOn = outputs.pumpEnabled && outputs.pumpSpeedPercent > 0;
-  int speedPercent = pumpOn ? constrain(outputs.pumpSpeedPercent, 0, 100) : 0;
+  int requestedSpeedPercent = pumpOn ? constrain(outputs.pumpSpeedPercent, 1, 100) : 0;
 
   if (PUMP_PWM_ENABLED == 1)
   {
     int dutyMax = (1 << PUMP_PWM_RESOLUTION_BITS) - 1;
-    int duty = map(speedPercent, 0, 100, 0, dutyMax);
+    int minDutyPercent = constrain(PUMP_PWM_MIN_DUTY_PERCENT, 0, 100);
+    int maxDutyPercent = constrain(PUMP_PWM_MAX_DUTY_PERCENT, minDutyPercent, 100);
+    int mappedDutyPercent = 0;
+
+    if (pumpOn)
+    {
+      mappedDutyPercent = map(requestedSpeedPercent, 1, 100, minDutyPercent, maxDutyPercent);
+    }
+
+    if (pumpOn && !lastPumpOn)
+    {
+      pumpStartupBoostActive = PUMP_STARTUP_BOOST_MS > 0;
+      pumpStartupBoostUntil = millis() + PUMP_STARTUP_BOOST_MS;
+    }
+
+    if (!pumpOn)
+    {
+      pumpStartupBoostActive = false;
+      pumpStartupBoostUntil = 0;
+    }
+
+    bool boostActiveNow = pumpStartupBoostActive && millis() < pumpStartupBoostUntil;
+
+    if (pumpStartupBoostActive && !boostActiveNow)
+    {
+      pumpStartupBoostActive = false;
+    }
+
+    int appliedDutyPercent = boostActiveNow
+                                 ? constrain(PUMP_STARTUP_BOOST_DUTY_PERCENT, 0, 100)
+                                 : mappedDutyPercent;
+    int duty = map(appliedDutyPercent, 0, 100, 0, dutyMax);
 
     if (PUMP_OUTPUT_ACTIVE_HIGH != 1)
     {
       duty = dutyMax - duty;
     }
 
-    ledcWrite(PUMP_PWM_CHANNEL, constrain(duty, 0, dutyMax));
+    duty = constrain(duty, 0, dutyMax);
+    ledcWrite(PUMP_PWM_CHANNEL, duty);
 
-    if (!hasLoggedPumpLevel || lastPumpOn != pumpOn)
+    if (!hasLoggedPumpLevel ||
+        lastPumpOn != pumpOn ||
+        lastPumpSpeedPercent != requestedSpeedPercent ||
+        lastPumpDuty != duty)
     {
       Serial.print("HardwareOutputs pump GPIO");
       Serial.print(PUMP_OUTPUT_PIN);
@@ -133,11 +190,17 @@ void HardwareOutputs::applyPump(const FountainOutputState &outputs)
       Serial.print(dutyMax);
       Serial.print(" pump=");
       Serial.print(pumpOn ? "ON" : "OFF");
-      Serial.print(" speed=");
-      Serial.println(speedPercent);
+      Serial.print(" requested_speed=");
+      Serial.print(requestedSpeedPercent);
+      Serial.print(" mapped_duty_percent=");
+      Serial.print(mappedDutyPercent);
+      Serial.print(" boost=");
+      Serial.println(boostActiveNow ? "yes" : "no");
 
       hasLoggedPumpLevel = true;
       lastPumpOn = pumpOn;
+      lastPumpSpeedPercent = requestedSpeedPercent;
+      lastPumpDuty = duty;
     }
 
     return;
@@ -159,7 +222,7 @@ void HardwareOutputs::applyPump(const FountainOutputState &outputs)
     Serial.print(" pump=");
     Serial.print(pumpOn ? "ON" : "OFF");
     Serial.print(" speed=");
-    Serial.println(speedPercent);
+    Serial.println(requestedSpeedPercent);
 
     hasLoggedPumpLevel = true;
     lastPumpOn = pumpOn;
