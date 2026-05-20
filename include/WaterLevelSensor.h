135e462e47df7dba8a42f9bdb884d73bd09b5b9a
bdkazal <bdkazal@gmail.com>
2026-05-20 18:23:25 +0600

    Debounce water level switch input

--- include/WaterLevelSensor.h
+++ include/WaterLevelSensor.h
@@ -13,7 +13,11 @@ private:
   bool switchConfigured = false;
   bool lastWaterLow = false;
   bool hasLoggedInitialState = false;
+  bool debouncedWaterLow = false;
+  bool lastRawSwitchWaterLow = false;
+  unsigned long lastRawSwitchChangedAt = 0;
 
   bool readSwitchWaterLow() const;
+  bool debouncedSwitchWaterLow();
   void applyWaterLowState(FountainReadings &readings, bool waterLow, const char *source);
 };
