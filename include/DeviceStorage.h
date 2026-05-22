14b682871774ce01c37c9757ec29d5b749d746db
bdkazal <bdkazal@gmail.com>
2026-05-22 22:35:33 +0600

    Add Wi-Fi credential storage

---
+++ include/DeviceStorage.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Arduino.h>
+
+struct StoredDeviceConfig
+{
+  String wifiSsid;
+  String wifiPassword;
+  bool hasWifiCredentials = false;
+};
+
+void beginDeviceStorage();
+
+StoredDeviceConfig loadStoredDeviceConfig();
+bool saveWifiCredentials(const String &ssid, const String &password);
+void clearStoredWifiCredentials();
+void requestWifiSetupPortalOnNextBoot();
+bool consumeWifiSetupPortalRequest();
