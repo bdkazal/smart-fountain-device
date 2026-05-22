73badcf39389133d371a0f4547fe3ee269ed3f1a
bdkazal <bdkazal@gmail.com>
2026-05-22 22:24:10 +0600

    Prepare production demo device config example

--- include/DeviceSecrets.example.h
+++ include/DeviceSecrets.example.h
@@ -1,16 +1,20 @@
 #pragma once
 
-// Copy this file to include/DeviceSecrets.h and fill in real local values.
+// Production demo config template for iot.biztola.com.
+//
+// Copy this file to include/DeviceSecrets.h before building/uploading:
+//
+//   cp include/DeviceSecrets.example.h include/DeviceSecrets.h
+//
+// Then fill in your real Wi-Fi credentials below.
 // Do not commit include/DeviceSecrets.h.
 
 #define WIFI_SSID "YOUR_WIFI_NAME"
 #define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
 
-// From ESP32, do not use 127.0.0.1.
-// Use your Mac/Laravel server LAN IP, for example: http://192.168.0.105:8000
-#define API_BASE_URL "http://192.168.0.xxx:8000"
+#define API_BASE_URL "https://iot.biztola.com"
 
-#define DEVICE_UUID "YOUR_SMART_FOUNTAIN_DEVICE_UUID"
-#define DEVICE_API_KEY "YOUR_SMART_FOUNTAIN_DEVICE_API_KEY"
+#define DEVICE_UUID "9f2a4b8e-6c7d-4f31-9a8e-2b5c0f7d1a64"
+#define DEVICE_API_KEY "bzt_prod_test_9Nq7xL4vR2mK8pQ6sT1yA3cD5eF7hJ0"
 
-#define FIRMWARE_VERSION "smart-fountain-dev-0.1"
+#define FIRMWARE_VERSION "smart-fountain-prod-0.1"
