83e3371f0bb4ecb3b34320979114d3063a8b42a7
bdkazal <bdkazal@gmail.com>
2026-06-20 00:34:26 +0600

    Turn HardwareOutputs into output driver facade

--- include/HardwareOutputs.h
+++ include/HardwareOutputs.h
@@ -2,6 +2,9 @@
 
 #include <Arduino.h>
 #include "FountainTypes.h"
+#include "PumpOutputDriver.h"
+#include "CobOutputDriver.h"
+#include "RgbOutputDriver.h"
 
 class HardwareOutputs
 {
@@ -19,28 +22,7 @@ public:
 
 private:
   bool hardwareEnabled = false;
-  bool hasLoggedPumpLevel = false;
-  bool lastPumpOn = false;
-  bool lastPumpAssistActive = false;
-  int lastPumpSpeedPercent = -1;
-  int lastPumpDuty = -1;
-  bool pumpStartupBoostActive = false;
-  unsigned long pumpStartupBoostUntil = 0;
-  unsigned long lastPumpAssistKickAt = 0;
-  bool hasLoggedCobLevel = false;
-  bool lastCobOn = false;
-  bool hasLoggedRgbLevel = false;
-  bool lastRgbOn = false;
-  int lastRgbBrightnessPercent = -1;
-  String lastRgbColor = "";
-  String lastRgbEffect = "";
-  int lastRgbRedDuty = -1;
-  int lastRgbGreenDuty = -1;
-  int lastRgbBlueDuty = -1;
-
-  void applyPump(const FountainOutputState &outputs);
-  void applyCob(const FountainOutputState &outputs);
-  void applyRgb(const FountainOutputState &outputs);
-  int rgbDutyFromChannel(int channelValue, int brightnessPercent) const;
-  void parseRgbColor(const String &hexColor, int &red, int &green, int &blue) const;
+  PumpOutputDriver pumpDriver;
+  CobOutputDriver cobDriver;
+  RgbOutputDriver rgbDriver;
 };
